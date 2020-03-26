9fd2ac6efb98c97d53ff741be101a7c316413a90
s-janibekova <49087652+s-janibekova@users.noreply.github.com>
2020-03-26 21:55:57 +0600

    delete

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,7 @@ import firestore from 'firebase/firestore'
 
   // Initialize Firebase
   var config = {
-    apiKey: 'AIzaSyBdQR0RIU_pWYWOHCCTV2GN9aw4vhIY9ts',
-    authDomain: 'chat-app-3ba0e.firebaseapp.com',
-    databaseURL: 'https://chat-app-3ba0e.firebaseio.com',
-    projectId: 'chat-app-3ba0e',
-    storageBucket: 'chat-app-3ba0e.appspot.com',
-    messagingSenderId: '975194806058',
-    appId: '1:975194806058:web:a83063f0b5a6f22fdf7053'
+    // put your config here
   };
   
   const firebaseApp = firebase.initializeApp(config);
