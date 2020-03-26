495f18c9ed0b5d41efedb741e481b6af6a82acac
saltanat@pingocean.com <saltanat@pingocean.com>
2020-03-26 20:30:48 +0600

    chatting works

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -34,22 +34,24 @@ export default {
       messages: []
     }
   },
-  created(){
+  created() {
     let ref = db.collection('messages').orderBy('timestamp')
-    
-    // subscribe to changes to the 'messages' collection
     ref.onSnapshot(snapshot => {
-      snapshot.docChanges.forEach(change => {
-        if(change.type == 'added'){
-          let doc = change.doc
-          this.messages.push({
-            id: doc.id,
-            name: doc.data().name,
-            content: doc.data().content,
-            timestamp: moment(doc.data().timestamp).format('lll')
-          })
-        }
+     
+        snapshot.docChanges.forEach(change => {
+           if(change.type == 'added'){
+             let doc = change.doc
+             this.messages.push({
+               id: doc.id,
+               name: doc.data().name,
+               content: doc.data().content,
+               timestamp:  moment(doc.data().timestamp).format('lll')
+             })
+           }
+           console.log(this.messages)
       })
+      
+
     })
   }
 }
@@ -67,5 +69,18 @@ export default {
   display: block;
   font-size: 0.8em;
 }
-</style>
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
+.messages::-webkit-scrollbar {
+  width: 3px;
+}
+
+.messages::-webkit-scrollbar-thumb{
+  background: gray;
+}
+
+
+</style>  
 
